18f5a4c49aed574454c20e7df55bacc0115b848e
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-25 10:58:40 +0800

    adds route, controller, and views for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :update_allowed_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+
+    # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    protected
+
+    def after_sign_in_path_for(resource_or_scope)
+        dashboard_path
+    end
+    
+    def after_sign_out_path_for(resource_or_scope)
+        new_user_session_path
+    end
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password)}
+    end
+
+    # def record_not_found
+    #     render file: Rails.root.join('public/404.html'), layout: true, status: :not_found
+    # end
 end
