6842627a2b9f10548093c849c3dd19a442541dd8
lkpallarca <Ckencepallarca@gmail.com>
2022-06-26 19:14:23 +0800

    add confirmation route controller and modify root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    root to: 'devise/sessions#new'
-  end
+  devise_for :users, controllers: {
+    confirmations: 'confirmations'
+  }
   
-  get '/dashboard', to: 'pages#index'
-
+  root to: 'pages#index'
 end
