82c211ed9b8b3fde7c6ed8709886a4c5d91763ed
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-30 20:01:04 +0800

    user can only access if approved

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,5 +1,12 @@
 class TransactionController < ApplicationController
-    def new
-        
+    before_action :approved
+
+    private
+
+    def approved
+        if !current_user.approved
+            flash[:notice] = "Your account must be approved to access this area."
+            redirect_to root_path unless current_user.approved?
+        end
     end
 end
