d0fd3eb3ae79ec282cebfcc871901a5b71984ee7
lkpallarca <Ckencepallarca@gmail.com>
2022-06-30 20:06:15 +0800

    add admin/transactions namespaced index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :transactions, only:[:index]
+  end
   devise_for :users, controllers: {
     confirmations: 'confirmations'
   }
