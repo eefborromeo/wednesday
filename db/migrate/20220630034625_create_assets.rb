5032fe8be4dfdb6caa30b8ed05eee4d844a5c832
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-06-30 12:12:42 +0800

    adds new migrations and schema

--- db/migrate/20220630034625_create_assets.rb
+++ db/migrate/20220630034625_create_assets.rb
@@ -1,7 +1,8 @@
 class CreateAssets < ActiveRecord::Migration[6.1]
   def change
     create_table :assets do |t|
-      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.string :asset_name
 
       t.timestamps
     end
--- db/migrate/20220630035504_create_transactions.rb
+++ db/migrate/20220630035504_create_transactions.rb
@@ -1,10 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration[6.1]
   def change
     create_table :transactions do |t|
-
-      t.timestamps
       t.references :user, null: false, foreign_key: true
       t.references :asset, null: false, foreign_key: true
+
+      t.timestamps
     end
   end
 end
