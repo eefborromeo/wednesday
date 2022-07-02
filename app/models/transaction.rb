262bd2eb978b53efad798dd5f1cf32ab5d69bdc8
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-02 09:43:51 +0800

    updates update_asset_total_shares to subtract total_shares

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,7 +7,11 @@ class Transaction < ApplicationRecord
     private 
 
     def update_asset_total_shares
-        asset = Asset.find_by(id: asset_id)
-        asset.update(total_shares: shares)
+        @asset = Asset.find_by(id: asset_id, user_id: user_id)
+        if transaction_type == "buy"
+            @asset.update(total_shares: asset.total_shares + shares)
+        else 
+            @asset.update(total_shares: asset.total_shares - shares)
+        end
     end
 end
