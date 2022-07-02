d8cc11684efa635770787d8bd13c5044d03b7b12
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-02 09:45:44 +0800

    adds sell feature

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -3,5 +3,27 @@ class SellController < ApplicationController
         @asset = Asset.find_by(asset_name: params[:id], user_id: current_user.id)
         @user = current_user.id
         @company_name = Asset.get_company_name(@asset.asset_name)
+        @latest_price = Asset.get_latest_price(@asset.asset_name)
+        @transaction = Transaction.new
+    end
+
+    def create
+        @transaction = Transaction.new(sell_params)
+
+        if @transaction.save
+            redirect_to transaction_index_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def sell_params
+        params[:transaction][:user_id] = current_user.id
+        params[:transaction][:user_email] = current_user.email
+        params[:transaction][:transaction_type] = 'sell'
+        
+        params.require(:transaction).permit(:user_id, :asset_id, :company_name, :asset_name, :asset_price, :transaction_type, :user_email, :shares)
     end
 end
