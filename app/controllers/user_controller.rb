a43a550a9382256dea44b04a34fc1e148fb2b155
lkpallarca <Ckencepallarca@gmail.com>
2022-07-04 21:22:07 +0800

    create user controller for settings feature

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,33 @@
+class UserController < ApplicationController
+  before_action :set_user
+
+  def edit
+  end
+
+  def edit_money
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def update_money
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+      params.require(:user).permit(:email, :password, :money)
+  end
+end
