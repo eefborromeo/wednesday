b24d50db1a621f29f7dce772b86ef66d9919a87c
lkpallarca <Ckencepallarca@gmail.com>
2022-07-05 14:38:33 +0800

    modify routes to include destroy paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,25 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :transactions, only:[:index]
-  end
-
   devise_for :users, controllers: {
     confirmations: 'confirmations'
   }
-  
+
   root to: 'wednesday#index'
+
+  namespace :admin do
+    resources :users
+    resources :transactions, only:[:index]
+  end
 
   resources :user, only: [:edit, :update]
 
   get '/user/:id/edit/money', to: 'user#edit_money', as: 'edit_user_money'
   patch '/user/:id/money', to: 'user#update_money', as: 'user_money'
 
-  resources :asset, except: [:destroy]
+  resources :asset
 
-  resources :transaction, only: [:index, :create]
+  resources :transaction, only: [:index, :create, :destroy]
   get '/transaction/new/:id' => 'transaction#new', as: 'new_transaction'
   
   resources :sell, only: [:create]
   get '/sell/new/:id' => 'sell#new', as: 'new_sell'
-
-  namespace :admin do
-    resources :users
-  end
 end
