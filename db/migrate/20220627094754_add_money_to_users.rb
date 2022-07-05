5c871234779bec55bf93ca18dd577d53685f02ca
lkpallarca <Ckencepallarca@gmail.com>
2022-07-05 11:23:12 +0800

    modify user money default value upon account create

--- db/migrate/20220627094754_add_money_to_users.rb
+++ db/migrate/20220627094754_add_money_to_users.rb
@@ -1,5 +1,5 @@
 class AddMoneyToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :money, :float, default: 0
+    add_column :users, :money, :float, default: 10000
   end
 end
