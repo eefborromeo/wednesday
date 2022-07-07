4a364f8d70182d7a86abcc37d07fdb8ca2868062
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-07 16:17:54 +0800

    adds navigation to admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    def active_class(control)
-        current_page?(controller: control) ? "bg-zinc-700 rounded-r-2xl" : ""
+    def active_class(control, action = "index")
+        current_page?(controller: control, action: action) ? "bg-zinc-700 rounded-r-2xl" : ""
     end
 end
