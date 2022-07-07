597ee89c969db21510b2abee3d50799846f21ec4
lkpallarca <Ckencepallarca@gmail.com>
2022-07-07 20:43:23 +0800

    delete spec helper folder and comment out pending tests

--- spec/requests/admin/transactions_spec.rb
+++ spec/requests/admin/transactions_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Admin::Transactions", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admin/transactions/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+# RSpec.describe "Admin::Transactions", type: :request do
+#   describe "GET /index" do
+#     it "returns http success" do
+#       get "/admin/transactions"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-end
+# end
--- spec/requests/admin/wednesday_spec.rb
+++ spec/requests/admin/wednesday_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Admin::Wednesdays", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
+# RSpec.describe "Admin::Wednesdays", type: :request do
+#   describe "GET /index" do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+# end
