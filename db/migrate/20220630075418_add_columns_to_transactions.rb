dada3c412ac34d7df98bbc311eadcb39d26f02b2
lkpallarca <Ckencepallarca@gmail.com>
2022-07-07 17:52:27 +0800

    modify migration to remove asset_id inside transaction table

--- db/migrate/20220630075418_add_columns_to_transactions.rb
+++ db/migrate/20220630075418_add_columns_to_transactions.rb
@@ -1,7 +1,6 @@
 class AddColumnsToTransactions < ActiveRecord::Migration[6.1]
   def change
     add_column :transactions, :company_name, :string
-    add_column :transactions, :asset_id, :bigint
     add_column :transactions, :asset_name, :string
     add_column :transactions, :asset_price, :decimal
     add_column :transactions, :shares, :decimal
