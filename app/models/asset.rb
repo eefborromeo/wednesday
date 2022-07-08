1d83bca58ce8653dc919f623f9ef907340c2cbe5
lkpallarca <Ckencepallarca@gmail.com>
2022-07-08 13:27:32 +0800

    refactor asset info retrieval from iex api and create get popular assets method

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,15 +3,35 @@ class Asset < ApplicationRecord
     
     validates :asset_name, :uniqueness => {:scope => :user_id}
 
+    def self.get_asset_info(asset_name)
+        { 
+            company_name: Asset.iex_api.company(asset_name).company_name, 
+            price: Asset.iex_api.quote(asset_name).latest_price 
+        }
+    end
+
+    def self.get_popular_assets
+        popular_assets = []
+        sample = [
+            'MSFT',
+            'META',
+            'AMZN',
+            'AAPL',
+            'NFLX',
+            'GOOG',
+            'JBFCF'
+        ]
+        sample.each do |each|
+            popular_assets << {
+                name: each,
+                company_name: Asset.iex_api.company(each).company_name,
+                price: Asset.iex_api.quote(each).latest_price
+            }
+        end
+        popular_assets
+    end
+
     def self.iex_api
         IEX::Api::Client.new
-    end
-
-    def self.get_latest_price(company)
-        Asset.iex_api.quote(company).latest_price
-    end
-
-    def self.get_company_name(company)
-        Asset.iex_api.company(company).company_name
     end
 end
