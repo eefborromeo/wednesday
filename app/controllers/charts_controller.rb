c4738774341e532f5d9486010ddc8f1521504ed4
lkpallarca <Ckencepallarca@gmail.com>
2022-07-10 16:56:54 +0800

    create charts controller for json chart endpoints

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,13 @@
+class ChartsController < ApplicationController
+  def new_users
+    render json: User.group_by_day(:created_at).count
+  end
+
+  def all_completed_transactions
+    render json: Transaction.group_by_day(:created_at).count
+  end
+
+  def asset_specific_transactions
+    render json: current_user.transactions.where(asset_name: params[:asset_name]).group_by_day(:created_at).count
+  end
+end
