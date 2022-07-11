b6220e8358810a54113a1a3853b29c48224a4e57
lkpallarca <Ckencepallarca@gmail.com>
2022-07-11 12:28:47 +0800

    create factories validator to check factory validity before running the whole test suite

---
+++ spec/factories_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+EXCLUDED = []
+
+FactoryBot.factories.map(&:name).each do |factory_name|
+  describe "the #{factory_name} factory" do
+    next if EXCLUDED.include?(factory_name)
+  
+    it "is valid" do
+      expect(build factory_name).to be_valid
+    end
+  end
+end
