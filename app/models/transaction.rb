d3dad0029525e4d25eba3dc8a5706aac47b4fb43
lkpallarca <Ckencepallarca@gmail.com>
2022-07-11 12:27:08 +0800

    refactor shares validation to reject negative values, and add presence validation for user email

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,22 +5,29 @@ class Transaction < ApplicationRecord
     after_save :update_asset_total_shares
     after_save :update_user_balance
 
+    validates_presence_of :user_email
+
     validates :shares,
-                format: { with: /\A-?+(?=.??\d)\d*\.?\d*\z/, message: 'Only allows numbers' }
+                numericality: {greater_than_or_equal_to: 0, message: "Buying or Selling only allows numbers."}
 
     private 
 
     def check_transaction_amount_validity
         user = User.find_by(id: user_id)
         asset = Asset.find_by(asset_name: asset_name, user_id: user_id)
+
+        if shares < 0
+            errors.add(:base, "You cannot #{transaction_type} negative shares.")
+            throw :abort
+        end
         
         if transaction_type == "buy" && user.money < (shares * asset_price)
-            errors.add(:base, 'Insufficient balance, please try again later.')
+            errors.add(:base, 'Insufficient balance, please try again.')
             throw :abort
         end
 
         if transaction_type == "sell" && shares > asset.total_shares
-            errors.add(:base, 'Insufficient number of shares, please try again later.')
+            errors.add(:base, 'Insufficient number of shares, please try again.')
             throw :abort
         end
     end
