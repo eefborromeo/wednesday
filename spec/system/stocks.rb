c9c2d40439c048258f37facf168ed4c8b944a685
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-12 17:06:43 +0800

    adds stocks file and sets up contexts

---
+++ spec/system/stocks.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Buying, selling, viewing stocks", type: :system do
+    context "confirmed and approved user" do
+    end
+
+    context "confirmed user" do
+    end
+end
