492849296f3a0f2693fa01e394342405fd42f1ba
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-12 18:42:58 +0800

    adds login and viewing stocks

--- spec/system/stocks.rb
+++ spec/system/stocks.rb
@@ -1,9 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Buying, selling, viewing stocks", type: :system do
+    let(:confirmed_and_approved_user) { create(:full_access_user) }
+    let(:confirmed_user) { create(:confirmed_user) }
+
+    
     context "confirmed and approved user" do
+        before do
+            login confirmed_and_approved_user
+            click_on "+ Add Asset"
+            fill_in "asset[asset_name]", with: "JBFCF"
+            click_on "Add"
+        end
+
+        scenario "viewing stocks" do
+            click_on "Portfolio"
+            expect(page).to have_content("JBFCF")
+            click_on "JBFCF"
+            expect(page).to have_content("Jollibee Foods Corp.")
+            expect(page).to have_content("JBFCF high market price for the last month")
+            expect(page).to have_content("Latest Headlines")
+        end
     end
 
     context "confirmed user" do
+    end
+
+    private
+
+    def login(user)
+        visit new_user_session_path
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Log in"
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content('Signed in successfully.')
     end
 end
