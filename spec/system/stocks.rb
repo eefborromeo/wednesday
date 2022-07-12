9b8d31733ea868dc2e75e2ddb3116835e92beb0d
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-12 19:22:43 +0800

    adds tests for confirmed only user

--- spec/system/stocks.rb
+++ spec/system/stocks.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Buying, selling, viewing stocks", type: :system do
+RSpec.describe "viewing, buying, and selling stocks", type: :system do
     let(:confirmed_and_approved_user) { create(:full_access_user) }
     let(:confirmed_user) { create(:confirmed_user) }
     
@@ -38,6 +38,27 @@ RSpec.describe "Buying, selling, viewing stocks", type: :system do
     end
 
     context "confirmed user" do
+        before do
+            login confirmed_user
+            click_on "+ Add Asset"
+            fill_in "asset[asset_name]", with: "JBFCF"
+            click_on "Add"
+        end
+
+        scenario "viewing stocks" do
+            expect(page).to have_content("You have successfully added JBFCF to your portfolio")
+            expect(page).to have_content("JBFCF")
+            click_on "JBFCF"
+            expect(page).to have_content("Jollibee Foods Corp.")
+            expect(page).to have_content("JBFCF high market price for the last month")
+            expect(page).to have_content("Latest Headlines")
+        end
+        
+        scenario "buying and selling stock" do
+            click_on "Buy Shares"
+            expect(page).to have_content("Your account must be approved to access this area.")
+            expect(page).not_to have_content("Sell Shares")
+        end
     end
 
     private
