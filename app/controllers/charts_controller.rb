cc5ecf9670bb715e8332c1775e20a2d040a26993
lkpallarca <Ckencepallarca@gmail.com>
2022-07-13 09:05:40 +0800

    add date pattern modification for chart display

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,13 @@
 class ChartsController < ApplicationController
   def new_users
-    render json: User.group_by_day(:created_at).count
+    render json: User.group_by_day(:created_at).count.map{ |k,v| [[k.strftime('%-B %-d')], v] }
   end
 
   def all_completed_transactions
-    render json: Transaction.group_by_day(:created_at).count
+    render json: Transaction.group_by_day(:created_at).count.map{ |k,v| [k.strftime('%-B %-d'), v] }
   end
 
   def asset_specific_transactions
-    render json: current_user.transactions.where(asset_name: params[:asset_name]).group_by_day(:created_at).count
+    render json: current_user.transactions.where(asset_name: params[:asset_name]).group_by_day(:created_at).count.map{ |k,v| [[k.strftime('%-B %-d')], v] }
   end
 end
