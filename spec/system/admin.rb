8a6eb4f3857e13ba52196de25b36382954758950
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-07-13 13:28:23 +0800

    adds admin spec

---
+++ spec/system/admin.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Admin functionalities", type: :system do
+    context "when creates new trader" do
+
+    end
+
+    context "when edits trader details" do
+
+    end
+
+    context "when views trader details" do
+
+    end
+
+    context "when views all traders in the app" do
+        it "will show pending trader sign up" do
+
+        end
+
+        it "will approve pending trader" do
+
+        end
+    end
+
+    context "when view all transactions" do
+
+    end
+end
